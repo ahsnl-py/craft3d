a14246777489a2a083b678afae2473a31ebcb547
ahsnl7 <ahsnl.mi@gmail.com>
2022-03-01 18:19:40 +0100

    temporary save for futher dev

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent }  from './app.component';
 import { CustomersModule } from './customers/customers.module';
+import { SharedModule } from  './shared/shared.module';
+import { AppComponent }  from './app.component';
 
 @NgModule({
-  imports:      [ BrowserModule, CustomersModule ],
+  imports:      [ BrowserModule, CustomersModule, SharedModule ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
