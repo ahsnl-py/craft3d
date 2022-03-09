f61b5a44bc3d787d4ed552168a7daa3fc1370f4a
ahsnl7 <ahsnl.mi@gmail.com>
2022-03-09 23:49:21 +0100

    updates to deploy to Heroku

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/craft-gui'));
+app.use(express.static(__dirname + '/dist/craft3d'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/craft-gui/index.html'));});
+'/dist/craft3d/index.html'));});
 app.listen(process.env.PORT || 8080);
